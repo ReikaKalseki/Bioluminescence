94004c52d28095d64d7861b2c53152828f7334ec
Reika <reikasminecraft@gmail.com>
2020-05-26 02:23:04 -0400

    Update 6 - 0.18

--- functions.lua
+++ functions.lua
@@ -3,6 +3,7 @@ require "config"
 
 require "__DragonIndustries__.mathhelper"
 require "__DragonIndustries__.world"
+require "__DragonIndustries__.color"
 
 function createSeed(surface, x, y) --Used by Minecraft MapGen
 	local seed = surface.map_gen_settings.seed
@@ -104,26 +105,6 @@ local function createEmptyAnimation()
 	  frame_count = 1,
 	  direction_count = 1,
 	}
-end
-
-function convertColor(argb, divideBy)
-	local blue = bit32.band(argb, 255)
-	local green = bit32.band(bit32.rshift(argb, 8), 255)
-	local red = bit32.band(bit32.rshift(argb, 16), 255)
-	if divideBy then
-		red = red/255
-		green = green/255
-		blue = blue/255
-	end
-	return {r = red, g = green, b = blue}
-end
-
-local function permuteColor(clr, dr, dg, db)
-	clr = table.deepcopy(clr)
-	clr.r = math.max(0, math.min(255, clr.r+dr))
-	clr.g = math.max(0, math.min(255, clr.g+dg))
-	clr.b = math.max(0, math.min(255, clr.b+db))
-	return clr
 end
 
 local function generateColorVariations(colors)
