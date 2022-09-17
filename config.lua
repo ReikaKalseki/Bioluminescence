19971c47eb4abbd9bf683013d9701c77aae5b6e9
Reika <reikasminecraft@gmail.com>
2022-09-17 18:18:47 -0400

    Update 17

--- config.lua
+++ config.lua
@@ -6,6 +6,8 @@ Config.glowPlants = settings.startup["glow-plants"].value
 
 Config.density = settings.startup["plant-density"].value
 
+Config.lightScale = settings.startup["light-scale"].value
+
 Config.scriptLight = settings.startup["script-lights"].value
 
 Config.retrogenDistance = -1
--- functions.lua
+++ functions.lua
@@ -28,7 +28,7 @@ end
 
 local function createBushLight(surface, entity, color)
 	if Config.scriptLight then
-		rendering.draw_light{sprite="utility/light_medium", scale=0.6, intensity=1, color=convertColor(RENDER_COLORS[color], true), target=entity, surface=surface}
+		rendering.draw_light{sprite="utility/light_medium", scale=0.6*Config.lightScale, intensity=1/math.sqrt(Config.lightScale), color=convertColor(RENDER_COLORS[color], true), target=entity, surface=surface}
 	else
 		createLightEntity(entity.surface, entity.position, 0.6, color)
 	end
@@ -49,7 +49,7 @@ end
 
 local function createLilyLight(surface, entity, color)
 	if Config.scriptLight then
-		rendering.draw_light{sprite="utility/light_medium", scale=0.5, intensity=1, color=convertColor(RENDER_COLORS[color], true), target=entity, surface=surface}
+		rendering.draw_light{sprite="utility/light_medium", scale=0.5*Config.lightScale, intensity=1/math.sqrt(Config.lightScale), color=convertColor(RENDER_COLORS[color], true), target=entity, surface=surface}
 	else
 		createLightEntity(entity.surface, entity.position, 0.5, color)
 	end
@@ -70,7 +70,7 @@ end
 
 local function createReedLight(surface, entity, color)
 	if Config.scriptLight then
-		rendering.draw_light{sprite="utility/light_medium", scale=0.7, intensity=1, color=convertColor(RENDER_COLORS[color], true), target=entity, surface=surface}
+		rendering.draw_light{sprite="utility/light_medium", scale=0.7*Config.lightScale, intensity=1/math.sqrt(Config.lightScale), color=convertColor(RENDER_COLORS[color], true), target=entity, surface=surface}
 	else
 		createLightEntity(entity.surface, entity.position, 0.7, color)
 	end
@@ -96,7 +96,7 @@ function createTreeLights(color, rand, entity, offset)
 		local rx = (rand(0, 10)-5)/10
 		local ry = (rand(0, 10)-5)/10
 		if Config.scriptLight then
-			rendering.draw_light{sprite="utility/light_medium", scale=1.0, intensity=1, color=convertColor(RENDER_COLORS[color], true), target=entity, target_offset = {rx+ox, ry+oy-d}, surface=entity.surface}				
+			rendering.draw_light{sprite="utility/light_medium", scale=1.0*Config.lightScale, intensity=1/math.sqrt(Config.lightScale), color=convertColor(RENDER_COLORS[color], true), target=entity, target_offset = {rx+ox, ry+oy-d}, surface=entity.surface}				
 		else
 			createLightEntity(entity.surface, {entity.position.x+rx+ox, entity.position.y+ry+oy-d}, 1.0, color)
 		end
@@ -145,7 +145,7 @@ function createBiterLight(entity)
 	if clr then
 		local box = entity.prototype.collision_box
 		local size = box and getBoundingBoxAverageEdgeLength(box)*1.2 or 0.5
-		rendering.draw_light{sprite="utility/light_medium", scale=size, intensity=1, color=clr, target=entity, surface=entity.surface}
+		rendering.draw_light{sprite="utility/light_medium", scale=size*Config.lightScale, intensity=1/math.sqrt(Config.lightScale), color=clr, target=entity, surface=entity.surface}
 		return true
 	end
 	--]]
@@ -267,7 +267,7 @@ local function createLight(name, sc, clr, collision)
 		circuit_wire_max_distance = 0,
 		signal_to_color_mapping = nil,
 		always_on = true,
-		light = {type = "basic", intensity = br, size = size*sc, color=clr},
+		light = {type = "basic", intensity = br/math.sqrt(Config.lightScale), size = size*sc*Config.lightScale, color=clr},
 	}
 end
 
